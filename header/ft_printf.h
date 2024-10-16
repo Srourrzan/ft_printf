305c154dc1b77c16413560db0faa0c4d15dbe305
Razan Srour <rsrour@c1r4s9.42amman.com>
2024-10-16 21:08:57 +0300

    hi there

---
+++ header/ft_printf.h
@@ -0,0 +1,7 @@
+#ifndef FT_PRINTF_H
+#define FT_PRINTF_H
+#include <limits.h>
+#include <stdio.h>
+#include <stdarg.h>
+int ft_printf(char *fmt, ...);
+#endif
---
+++ test.c
@@ -0,0 +1,7 @@
+#include "header/ft_printf.h"
+#include "header/libft.h"
+
+int main(void)
+{
+    ft_printf("hi there\n%s", "hi there");
+}
